ee88dfc027fc0312e672aca49fff4ab4a7dcce95
Naincy224 <pihusingh224@gmail.com>
2021-11-03 08:47:06 +0530

    Adding uc1 program for creating line and calculate its in LineComparisLineComparisonComputation

--- src/com/bridgeit/main/LineCompComput.java
+++ src/com/bridgeit/main/LineCompComput.java
@@ -7,6 +7,14 @@ public class LineCompComput {
 		System.out.println("Welcome to Line Comparison Computation Program");
 
 	}
+	
+	public double makeLine(Point point1, Point point2) {
+		
+		double length= Math.sqrt( (point2.getX() - point1.getX()) ^ 2 + (point2.getY()- point1.getY()) ^ 2);
+		
+		return length;
+		
+	}
 		
 }
 
