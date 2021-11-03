2d932c19e48fca81c60d9946e694d7f2449c2daa
Naincy224 <pihusingh224@gmail.com>
2021-11-03 08:35:54 +0530

    Adding Welcome message program  to LineComparisonComputation

---
+++ src/com/bridgeit/main/LineCompComput.java
@@ -0,0 +1,16 @@
+package com.bridgeit.main;
+
+public class LineCompComput {
+	
+	public void printWelcomeMessage() {
+
+		System.out.println("Welcome to Line Comparison Computation Program");
+
+	}
+		
+}
+
+	
+	
+	
+
---
+++ src/com/bridgeit/main/Mainclass.java
@@ -0,0 +1,14 @@
+package com.bridgeit.main;
+
+public class Mainclass {
+	
+	
+	public static void main(String[] args) {
+
+		LineCompComput lineObj = new LineCompComput();
+		lineObj.printWelcomeMessage();
+		
+	}
+
+
+}
