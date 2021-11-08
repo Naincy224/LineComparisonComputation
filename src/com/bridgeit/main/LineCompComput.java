b2f513cb0299dbe4c9946bc65cf1ec961a9a8b48
Naincy224 <pihusingh224@gmail.com>
2021-11-08 17:01:03 +0530

    modified UC1 program for linecomputation program

--- src/com/bridgeit/main/LineCompComput.java
+++ src/com/bridgeit/main/LineCompComput.java
@@ -1,24 +1,20 @@
 package com.bridgeit.main;
 
 public class LineCompComput {
-	
+
 	public void printWelcomeMessage() {
 
 		System.out.println("Welcome to Line Comparison Computation Program");
 
 	}
-	
+//Adding method to make line
 	public double makeLine(Point point1, Point point2) {
-		
-		double length= Math.sqrt( (point2.getX() - point1.getX()) ^ 2 + (point2.getY()- point1.getY()) ^ 2);
-		
-		return length;
-		
+
+		double length1 = Math.sqrt((point2.getX() - point1.getX()) ^ 2 + (point2.getY() - point1.getY()) ^ 2);
+
+		return length1;
+
 	}
-		
+
+	
 }
-
-	
-	
-	
-
--- src/com/bridgeit/main/Mainclass.java
+++ src/com/bridgeit/main/Mainclass.java
@@ -5,15 +5,20 @@ public class Mainclass {
 	
 	public static void main(String[] args) {
 
-		LineCompComput lineObj = new LineCompComput();
-
-		lineObj.printWelcomeMessage();
-		Point point1=new Point(2,3);
-		Point point2=new Point(5,7);
-		double length=lineObj.makeLine(point1, point2);
-		System.out.println("The Line length is:"+length);
 		
+		  LineCompComput lineObj = new LineCompComput();
+		  
+		  
+		  
+		  lineObj.printWelcomeMessage();
+		  Point point1=new Point(2,3);
+		  Point point2=new Point(5,7);
+		  
+		  
+		  double length1=lineObj.makeLine(point1, point2);
+		  System.out.println("The Line length is:"+length1);
+		  
+
+
 	}
-
-
 }
